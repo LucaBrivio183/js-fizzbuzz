8bb3f3ba1b888927478200f9b1bd46cc91f0fa0e
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-23 15:51:38 +0100

    feat: printed on screen by creating a list

--- js/main.js
+++ js/main.js
@@ -2,20 +2,32 @@
 
 'use strict'
 
-//creating  cycle for 
+//defining  ul variable
+
+const uList = document.querySelector("ul.unstyled-list");
+
+//creating  cycle for (1 to 100)
 
 for ( let i = 1; i <= 100; i++ ){
 
+     const liElement = document.createElement('li');
+
     if(  i % 15 === 0 ) {
         console.log('fizzbuzz');
+        liElement.append('fizzbuzz');
 
     }  else if( i % 5 === 0) {
         console.log('buzz');
+        liElement.append('buzz');
 
     } else if( i % 3 === 0 ) {
         console.log('fizz');
+        liElement.append('fizz');
 
     } else {
         console.log(i);
+        liElement.append(i);
     }
+
+    uList.append(liElement);
 }
