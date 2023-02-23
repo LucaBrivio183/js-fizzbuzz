e1dd9497df9b579aa131d181bdd680eb2c96e1b2
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-23 16:52:57 +0100

    feat: class added  to li based on index, styled

--- js/main.js
+++ js/main.js
@@ -13,21 +13,30 @@ for ( let i = 1; i <= 100; i++ ){
      const liElement = document.createElement('li');
 
     if(  i % 15 === 0 ) {
+        let classIndex = 'fizzbuzz';
         console.log('fizzbuzz');
         liElement.append('fizzbuzz');
+        liElement.classList.add(`${classIndex}`);
 
     }  else if( i % 5 === 0) {
+        let classIndex = 'buzz';
         console.log('buzz');
         liElement.append('buzz');
+        liElement.classList.add(`${classIndex}`);
 
     } else if( i % 3 === 0 ) {
+        let classIndex = 'fizz';
         console.log('fizz');
         liElement.append('fizz');
+        liElement.classList.add(`${classIndex}`);
 
     } else {
+        let classIndex = 'number';
         console.log(i);
         liElement.append(i);
+        liElement.classList.add(`${classIndex}`);
     }
 
     uList.append(liElement);
 }
+
